a62513fb93896de31250bc0302027cb117cfe8e9
Kei@Glou-Glout <menschenjager.mark.neun@gmail.com>
2020-04-27 17:17:07 +0900

    [update] add Graphic class

---
+++ src/Graphic/Graphic.cpp
@@ -0,0 +1,77 @@
+#include <Graphic/Graphic.hpp>
+#include <Event/UserInput.hpp>
+#include <Components/Head.hpp>
+#include <Components/Body.hpp>
+#include <Components/Food.hpp>
+#include <Components/Position.hpp>
+
+Graphic::Graphic(entt::registry& registry, entt::dispatcher& dispatcher)
+    : registry_(registry), dispatcher_(dispatcher),
+      window_(sf::VideoMode(400, 400), "Snake")
+{
+
+}
+
+void Graphic::step()
+{
+    sf::Event event;
+    while(window_.pollEvent(event))
+    {
+        if(event.KeyPressed)
+        {
+            switch(event.key.code)
+            {
+                case sf::Keyboard::K :
+                    dispatcher_.trigger<USERINPUT>(UP);
+                    break;
+                case sf::Keyboard::J :
+                    dispatcher_.trigger<USERINPUT>(DOWN);
+                    break;
+                case sf::Keyboard::L :
+                    dispatcher_.trigger<USERINPUT>(RIGHT);
+                    break;
+                case sf::Keyboard::H :
+                    dispatcher_.trigger<USERINPUT>(LEFT);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    window_.clear(sf::Color::White);
+    auto head_view = registry_.view<Head, Position>();
+
+    for(auto entity : head_view)
+    {
+        auto head_pos = registry_.get<Position>(entity);
+        sf::RectangleShape head;
+        head.setPosition(head_pos.x * 2, head_pos.y * 2);
+        head.setFillColor(sf::Color::Yellow);
+        window_.draw(head);
+    }
+
+    auto body_view = registry_.view<Body, Position>();
+
+    for(auto entity : body_view)
+    {
+        auto body_pos = registry_.get<Position>(entity);
+        sf::RectangleShape body;
+        body.setPosition(body_pos.x * 2, body_pos.y * 2);
+        body.setFillColor(sf::Color::Green);
+        window_.draw(body);
+    }
+
+    auto food_view = registry_.view<Food, Position>();
+
+    for(auto entity : food_view)
+    {
+        auto food_pos = registry_.get<Position>(entity);
+        sf::RectangleShape food;
+        food.setPosition(food_pos.x * 2, food_pos.y * 2);
+        food.setFillColor(sf::Color::Red);
+        window_.draw(food);
+    }
+
+    window_.display();
+}
--- src/Graphic/Graphic.hpp
+++ src/Graphic/Graphic.hpp
@@ -1,8 +1,15 @@
 #pragma once
+#include <entt/entt.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 class Graphic
 {
+    entt::registry& registry_;
+    entt::dispatcher& dispatcher_;
+    sf::RenderWindow window_;
+
 public:
-    Graphic();
+    Graphic(entt::registry& registry, entt::dispatcher& dispatcher);
     void step();
 };
